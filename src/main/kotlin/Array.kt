f26d0ce8c3ede1c4a885c286567c7cd747d3bb08
salik78 <77449919+salik78@users.noreply.github.com>
2022-08-11 12:25:16 +0530

    Array

--- src/main/kotlin/Array.kt
+++ src/main/kotlin/Array.kt
@@ -1,6 +1,7 @@
 fun main(){
     var a= arrayOf<Int>(1,2,3);
     var str = arrayOf<String>("Salik","Nikhil")
-    a.set(0,5)
-    println(a.get(0))
+    for(i in 0..a.size-1){
+        println(a[i])
+    }
 }
