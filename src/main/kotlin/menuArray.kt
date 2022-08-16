384746ac6296e1a0a8a7f99daabc183927f1e0f4
salik78 <77449919+salik78@users.noreply.github.com>
2022-08-16 12:25:58 +0530

    menu driven

---
+++ src/main/kotlin/menuArray.kt
@@ -0,0 +1,32 @@
+import java.util.Scanner
+
+fun main(){
+    var sc=Scanner(System.`in`);
+    var arr=Array<Int>(20){0};
+    while (true){
+        println("Enter user input");
+        var inp:Int=sc.nextInt();
+        if (inp==1){
+            println("Enter index no:");
+            var index:Int=sc.nextInt();
+            println("Enter element to insert")
+            var ele:Int=sc.nextInt();
+            arr.set(index,ele);
+        }
+        else if (inp==2){
+            println("Output is :");
+            for (i in 0 .. arr.size-1){
+                if (arr[i]==0){
+                    continue;
+                }
+                else{
+                    println(arr[i]);
+                }
+            }
+        }
+        else if(inp==3){
+            println("Exiting");
+            break;
+        }
+    }
+}
