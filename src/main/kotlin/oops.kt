15c09fb2413129c55fa2bb11c53473293d8b1021
salik78 <77449919+salik78@users.noreply.github.com>
2022-08-17 12:32:15 +0530

    oops1

---
+++ src/main/kotlin/oops.kt
@@ -0,0 +1,16 @@
+class Car{
+    var modelId:Int=13;
+    var name="";
+
+
+
+
+}
+
+fun main(){
+        var obj=Car();
+        var obj2=Car();
+        obj.modelId=12;
+        obj2.name="BMW";
+        println(obj.modelId)
+}
